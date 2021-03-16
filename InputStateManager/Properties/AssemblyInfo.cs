ee003ba71b50159cefadf128a28287d39224c7d0
Adam Johnson <adam.johnson@corteva.com>
2021-03-16 09:10:35 -0500

    updated to net5

--- InputStateManager/Properties/AssemblyInfo.cs
+++ InputStateManager/Properties/AssemblyInfo.cs
@@ -1,17 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("InputStateManager")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Unterrainer Informatik OG")]
-[assembly: AssemblyProduct("InputStateManager")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("This is a helper class for MonoGame useful for querying keyboard, mouse, touch and gamepad inputs.")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -20,16 +9,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("0c4c545b-94c2-4ad2-8cf2-be659dd85b10")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.1.4")]
-[assembly: AssemblyFileVersion("1.2.1.4")]
